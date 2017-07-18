27aa3050e80c5a0087e66facc49e503882fd84b2
An Le <anle@ip-10-0-0-17.ec2.internal>
2017-07-17 23:30:05 -0400

    edit ID

--- products.js
+++ products.js
@@ -1,13 +1,28 @@
 const _ = require('lodash')
 
-var products = [];
-
-const name = ['product1', 'product2', 'product3']
-const description = ['product1 description', 'product2 description', 'product3 description']
-const rating = [20, 45, 67]
+var products = [
+  {
+    id: 1,
+    name: 'product1',
+    description: 'product1',
+    rating: 35
+  },
+  {
+    id: 2,
+    name: 'product2',
+    description: 'product2',
+    rating: 50
+  },
+  {
+    id: 3,
+    name: 'product3',
+    description: 'product3',
+    rating: 60
+  }
+];
 
 function add (name, description, rating) {
-  products.push({ name: name, description: description, rating: rating, id: products.length });
+  products.push({ name: name, description: description, rating: rating, id: Math.round(1000*Math.random()) });
   return _.clone(products[products.length - 1]);
 }
 
@@ -32,7 +47,3 @@ function deleteProduct(id) {
 }
 
 module.exports = { add: add, list: list, find: find, max_rating: max_rating, deleteProduct: deleteProduct };
-
-for (let i = 0; i < 3; i++) {
-  module.exports.add(name[i], description[i], rating[i]);
-}
